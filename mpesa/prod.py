49c0cf50089febdc247be55b8fbe7955cb0cdf70
vinchuli <mavenwechuli@gmail.com>
2021-09-18 05:16:50 +0300

    deploy setup runtime fix

--- mpesa/prod.py
+++ mpesa/prod.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
